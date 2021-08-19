b67f04b7c3392021bec8102b83751dc8ae06fc51
Mohamed Hesham <mohamed.hesham@thirdwayv.com>
2021-08-19 21:44:25 +0200

    no message

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a671e7e36b79b40afb90ec831d76d312-437491834.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
